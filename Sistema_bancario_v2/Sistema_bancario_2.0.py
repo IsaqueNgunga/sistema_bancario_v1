8e4a8dd4a6e7edb4547d413736ba61b7fde6e320
Isaque Ngunga <isaquegit@hotmail.com>
2024-04-03 01:42:08 -0300

    Compreendendo a lógica

--- Sistema_bancario_v2/Sistema_bancario_2.0.py
+++ Sistema_bancario_v2/Sistema_bancario_2.0.py
@@ -1,1 +1,38 @@
-print("breve")
+def mensagem():
+    banco = "DS BANK"
+    mensagem = f"\n Seja bem vindo a {banco}! \n"
+    return mensagem
+
+def menu():
+    menu = """
+ ================= MENU =================
+
+               [1] Extrato
+               [2] Depositar
+               [3] Sacar
+               [0] Sair
+
+ ========================================
+ => """
+    return input(f"\n Como posso lhe ajudar? \n {menu}")
+
+saldo = 0
+limite = 500
+numero_saques = 0
+extrato = ""
+def xextrato(saldo, /, *, extrato):
+    print("\n================= EXTRATO =================")
+    print("Não foram realizadas movimentaçoes." if not extrato else extrato)
+    print(f"\n Saldo: R$ {saldo:.2f}")
+
+def main():
+ 
+ while True:
+  opcao = menu()
+
+  if opcao == "1":
+    xextrato(saldo, extrato=extrato)
+
+  elif opcao == "0":
+     break
+main()
