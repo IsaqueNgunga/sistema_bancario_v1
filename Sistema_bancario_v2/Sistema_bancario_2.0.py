077670ed87dac437a8a3f3fad1876a5518c078a4
Isaque Ngunga <isaquegit@hotmail.com>
2024-04-25 01:17:16 -0300

    Correção

--- Sistema_bancario_v2/Sistema_bancario_2.0.py
+++ Sistema_bancario_v2/Sistema_bancario_2.0.py
@@ -12,56 +12,62 @@ def menu():
 
  ========================================
  => """
-    return input(f"\n Como posso lhe ajudar? \n {menu}")
+    return input(f"\n Seja bem vindo a DS Bank! \n\n Como posso lhe ajudar? \n {menu}")
 
 def extract(saldo, /, *, extrato):
     print("\n================= EXTRATO =================\n")
-    print("Não foram realizadas movimentaçoes." if not extrato else extrato)
-    print(f"\n Saldo: R$ {saldo:.2f}")
+    print("\n Não foram realizadas movimentaçoes." if not extrato else extrato)
+    print(f"\n Saldo: R$ {saldo:.2f} \n\n Você será redirecionado ao menu principal.")
+    print("\n===========================================\n")
 
 def deposit(saldo,valor,extrato,/):
    
    if valor > 0:
       saldo += valor
-      extrato += f"\nDeposito: R$ {valor: .2f}\n"
-      return saldo, extrato
+      extrato += f"\n Deposito: R$ {valor: .2f}\n"
+      print("\n Depositgo relaizado! \n\n Você será redirecionado ao menu principal.\n")
+      
+   else:
+        print("\n Opção indisponível. \n\n Favor digitar um valor númerico positivo. \n\n Você será redirecionado ao menu principal. \n")
+
+   return saldo, extrato
 
 def withdraw(*,saldo, valor, extrato, SAQUES_DIARIOS, LIMIT_SAQUE, numero_saques):
    saque_indisponivel = numero_saques >= SAQUES_DIARIOS
    limit_exedido = valor > LIMIT_SAQUE
    
    if limit_exedido:
-            print("Limite exedido! Saque R$500,00 ou menos.")
+            print("\n Limite exedido! \n\n Saque R$500,00 ou menos. \n\n Você será redirecionado ao menu principal.")
 
    elif valor < 0:
-            print("Operação inválida, digite um valor positivo")
+            print("\n Operação inválida! \n\n igite um valor positivo. \n\n Você será redirecionado ao menu principal.")
 
    elif saque_indisponivel:
-            print("Saques diários esgotados! Aguarde 24h para realizar uma nova trasação.")
+            print("\n Saques diários esgotados! \n\n Aguarde 24h para realizar uma nova transação. \n\n Você será redirecionado ao menu principal.")
 
    elif valor <= saldo:
             saldo -= valor
             extrato += f"\nSaque: R$ {valor:.2f}\n"
             numero_saques += 1
-            print(f"\nTransação realizada. Valor: R$ {valor:.2f} Retire seu dinheiro na boca do caixa.\n")
+            print(f"\n Transação realizada. \n\n Valor: R$ {valor:.2f} \n\n Retire seu dinheiro na boca do caixa. \n\n Você será redirecionado ao menu principal.\n")
             
 
    elif valor > saldo:
-            print("Saldo insuficiente.")
+            print("\n Saldo insuficiente. \n\n Você será redirecionado ao menu principal.")
     
    return saldo, extrato, numero_saques
 
 def cadastro(usuarios):
      usuario = []
      nome = input("\n Informe seu nome completo:")
-     d_nacimento = input("\n Informe sua data de nacimento:")
-     cpf = input("\n Informe seu cpf:")
-     endereco = input("\n Informe seu endereço:")
+     d_nacimento = input("\n\n Informe sua data de nacimento:")
+     cpf = input("\n\n Informe seu cpf:")
+     endereco = input("\n\n Informe seu endereço:")
      
-     add_nome = usuario.append(nome)
-     add_nacimento = usuario.append(d_nacimento)
-     add_cpf = usuario.append(cpf)
-     add_endereco = usuario.append(endereco)
+     add_nome = usuario.append({"Nome": nome})
+     add_nacimento = usuario.append({"Data de Nacimento": d_nacimento})
+     add_cpf = usuario.append({"CPF": cpf})
+     add_endereco = usuario.append({"Endereço": endereco})
 
      print(f"\n Seja bem vindo a DS Banck {nome}!\n\n Abaixo dados cadastrais\n\n {usuario}")
 
@@ -85,11 +91,11 @@ def main():
     extract(saldo, extrato=extrato)
 
   elif opcao == "2":
-      valor = float(input("Informe o valor do deposito:"))
-      saldo, extrato = deposit(saldo, valor, extrato)
+      valor = float(input(" Informe o valor do deposito:"))
+      saldo,extrato = deposit(saldo, valor, extrato)
   
   elif opcao == "3":
-      valor = float(input("Informe o valor do saque: "))
+      valor = float(input(" Informe o valor do saque: "))
       
       saldo, extrato, numero_saques = withdraw(
            saldo=saldo,
